4506b3f118af746efd3ddf77a0857eee7ab9e68a
Daniel Wirtz <daniel@code-emitter.com>
2013-03-08 03:19:06 +0100

    ZIP my a...

--- scripts/configure.js
+++ scripts/configure.js
@@ -24,21 +24,23 @@ var ClosureCompiler = require(__dirname+"/../ClosureCompiler.js"),
     fs = require("fs"),
     path = require("path"),
 	child_process = require("child_process"),
-    pkg = require(__dirname+"/../package.json");
+    pkg = require(__dirname+"/../package.json"),
+    zlib = require("zlib"),
+    tar = require("tar");
 
 console.log("Configuring ClosureCompiler.js "+pkg.version+" ...\n");
 
 // Closure Compiler download url
-var ccUrl = "http://closure-compiler.googlecode.com/files/compiler-latest.zip";
+var ccUrl = "http://closure-compiler.googlecode.com/files/compiler-latest.tar.gz";
 
 // Temporary file for the download
-var ccTempFile = path.normalize(__dirname+path.sep+".."+path.sep+"compiler"+path.sep+"compiler.zip");
+var ccTempFile = path.normalize(__dirname+path.sep+".."+path.sep+"compiler"+path.sep+"compiler.tar.gz");
 
 // Bundled JRE download url
-var jreUrl = "http://bundled-openjdk-jre.googlecode.com/files/OpenJDK-JRE-7u6_24.zip";
+var jreUrl = "http://bundled-openjdk-jre.googlecode.com/files/OpenJDK-JRE-7u6_24.tar.gz";
 
 // Temporary file for the download
-var jreTempFile = path.normalize(__dirname+path.sep+".."+path.sep+"jre"+path.sep+"OpenJDK-JRE.zip");
+var jreTempFile = path.normalize(__dirname+path.sep+".."+path.sep+"jre"+path.sep+"jre.tar.gz");
 
 console.log("  Downloading Closure Compiler ...");
 var lastBytes = 0, currentBytes = 0, mb = 1024*1024;
@@ -55,8 +57,11 @@ download(ccUrl, ccTempFile, function(error, bytes) {
             console.log("  ✖ Unpack failed: "+error+"\n");
             fail();
         }
+        require("sleep").sleep(1); // Let the entry callbacks finish
         console.log("  ✔ Unpack complete.\n");
         configure_jre();
+    }, function(entry) {
+        console.log("  | "+entry["path"]);
     });
 }, function(bytes, total) {
     currentBytes += bytes;
@@ -79,33 +84,41 @@ function configure_jre() {
             // Travis CI for example has one, so we save their bandwidth. And Google's. And yours. And...
             finish();
         } else {
-            console.log("  ✖ Global Java not found, we need to install the bundled JRE ...");
-            console.log("    Downloading "+jreUrl+" ...");
-            lastBytes = 0; currentBytes = 0;
-            download(jreUrl, jreTempFile, function(error, bytes) {
-                if (error) {
-                    console.log("    ✖ Download failed: "+error+"\n");
-                    fail();
-                }
-                console.log("    ✔ Download complete: "+jreTempFile+" ("+parseInt(bytes/mb, 10)+" mb)\n");
-                require("sleep").sleep(1);
-                console.log("      Unpacking "+jreTempFile+" ...");
-                unpack(jreTempFile, function(error) {
+            if (fs.existsSync(__dirname+path.sep+".."+path.sep+"jre"+path.sep+"bin")) {
+                console.log("  ✖ Global Java not found, so let's test our bundled one ...\n");
+                runTest();
+            } else {
+                console.log("  ✖ Global Java not found, we need to download the bundled JRE ...");
+                console.log("    Downloading "+jreUrl+" ...");
+                lastBytes = 0; currentBytes = 0;
+                download(jreUrl, jreTempFile, function(error, bytes) {
                     if (error) {
-                        console.log("    ✖ Unpack failed: "+error+"\n");
+                        console.log("    ✖ Download failed: "+error+"\n");
                         fail();
                     }
-                    console.log("    ✔ Unpack complete.\n");
-                    configure();
-                    runTest();
+                    console.log("    ✔ Download complete: "+jreTempFile+" ("+parseInt(bytes/mb, 10)+" mb)\n");
+                    require("sleep").sleep(1);
+                    console.log("      Unpacking "+jreTempFile+" ...");
+                    unpack(jreTempFile, function(error) {
+                        if (error) {
+                            console.log("      ✖ Unpack failed: "+error+"\n");
+                            fail();
+                        }
+                        require("sleep").sleep(1); // Let the entry callbacks finish
+                        console.log("      ✔ Unpack complete.\n");
+                        configure();
+                        runTest();
+                    }, function(entry) {
+                        console.log("      | "+entry["path"]);
+                    });
+                }, function(bytes, total) {
+                    currentBytes += bytes;
+                    if (currentBytes == bytes || currentBytes - lastBytes >= mb) {
+                        console.log("    | "+parseInt(currentBytes/mb, 10)+" / "+(total > 0 ? parseInt(total/mb, 10) : "???")+" mb");
+                        lastBytes = currentBytes;
+                    }
                 });
-            }, function(bytes, total) {
-                currentBytes += bytes;
-                if (currentBytes == bytes || currentBytes - lastBytes >= mb) {
-                    console.log("    | "+parseInt(currentBytes/mb, 10)+" / "+(total > 0 ? parseInt(total/mb, 10) : "???")+" mb");
-                    lastBytes = currentBytes;
-                }
-            });
+            }
         }
     });
 }
@@ -119,7 +132,7 @@ function configure_jre() {
  */
 function download(downloadUrl, filename, callback, ondata) {
     var url = require("url").parse(downloadUrl);
-    var out = require("fs").createWriteStream(filename, { flags: 'w', encoding: "ascii", mode: 0666 });
+    var out = require("fs").createWriteStream(filename, { flags: 'w', encoding: null, mode: 0666 });
     var bytes = 0, total = -1;
     var req = require("http").request({
         "hostname": url["host"],
@@ -131,7 +144,7 @@ function download(downloadUrl, filename, callback, ondata) {
             total = parseInt(res.headers["content-length"], 10);
         }
         if (res.statusCode != 200) {
-            res.setEncoding("ascii");
+            res.setEncoding(null);
             callback(new Error("Download failed: HTTP status code "+res.statusCode), -1);
             return;
         }
@@ -155,16 +168,53 @@ function download(downloadUrl, filename, callback, ondata) {
  * Unpacks a file in place.
  * @param {string} filename File name
  * @param {function(?Error)} callback
+ * @param {function(Object)=} entryCallback
  */
-function unpack(filename, callback) {
-    try {
-        var AdmZip = require("adm-zip");
-        var zip = new AdmZip(filename);
-        zip.extractAllTo(path.dirname(filename), true);
-        callback(null);
-    } catch (e) {
-        callback(e);
-    }   
+function unpack(filename, callback, entryCallback) {   
+    var input = fs.createReadStream(filename, { flags: 'r', encoding: null }),
+        files = {},
+        dir = path.dirname(filename),
+        returned = false;
+    
+    input.pipe(zlib.createGunzip()).pipe(tar.Parse()).on("entry", function(entry) {
+        if (entryCallback) entryCallback(entry);
+        if (entry["type"] == 'File') {
+            files[entry["path"]] = fs.createWriteStream(dir+path.sep+entry["path"], { flags: 'w', encoding: null });
+            entry.pipe(files[entry["path"]]);
+            entry.on("end", function() {
+                files[entry["path"]].end();
+                files[entry["path"]]["done"] = true;
+                var alldone = true;
+                var names = Object.keys(files);
+                for (var i=0; i<names.length; i++) {
+                    if (!files[names[i]]["done"]) {
+                        alldone = false;
+                        break;
+                    }
+                }
+                if (alldone && !returned) {
+                    returned = true;
+                    callback(null);
+                }
+            });
+        } else if (entry["type"] == "Directory") {
+            try {
+                fs.mkdirSync(dir+path.sep+entry["path"]);
+            } catch (e) {
+                if (!fs.existsSync(dir+path.sep+entry["path"])) {
+                    if (!returned) {
+                        returned = true;
+                        callback(e);
+                    }
+                }
+            }
+        }
+    }).on("error", function(e) {
+        if (!returned) {
+            returned = true;
+            callback(e);
+        }
+    });
 }
 
 /**
